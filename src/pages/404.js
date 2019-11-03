3b8a4a93716d437e22d147e17907406af2beb8b7
Kevin Vo <kevinvo321@gmail.com>
2019-11-02 23:53:53 -0700

    add about section

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,20 @@
 import React from "react"
-
+import { Link } from "gatsby"
+import Background from "../components/background"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "./index.css"
 
 const NotFoundPage = () => (
-  <Layout>
+  <React.Fragment>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Background/>
+    <div style={{padding: "64px"}}>
+      <h1>404</h1>
+      <h3> PAGE NOT FOUND :(</h3>
+      <Link style={{color: "#999"}} to="/">Get me outta here 🤢</Link>
+    </div>
+  </React.Fragment>
 )
 
 export default NotFoundPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -45,8 +45,18 @@ const IndexPage = () => (
             isClickToPauseDisabled={true}/>
         </AnchorLink>
       </div>
-      <div id="about">
-      </div>
+    </div>
+    <div className="content" id="about">
+      <h2>I don't make things for people. I make things with people.</h2>
+
+      <p>
+        Beautiful things happen when we collaborate. I enjoy enabling others
+        to create by bridging vision with technology. I also love communicating
+        with others to see what their
+        goals are, and then expressing those goals through
+        elegant software solutions. So what do you say?
+      </p>
+      <AnchorLink style={{fontSize: "18px"}} href='#about'>Let's build something.</AnchorLink>
     </div>
   </React.Fragment>
 
