8de234b2ee4a34341e2b0689a5aac41f64fe9e1f
Kevin Vo <kevinvo321@gmail.com>
2019-11-11 12:02:38 -0800

    back button for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from "gatsby"
 
 const headerStyle = {
@@ -16,21 +16,34 @@ const contentStyle = {
   width: "100%",
 }
 
-const Header = () => (
-  <div style={headerStyle}>
-    <div className="header-container" style={contentStyle}>
-      <Link to="/">
-        <img
-          style={{margin: 0, height: "100%"}}
-          src={require("../images/kv_transparent.svg")} alt="logo"/>
-      </Link>
-      <div style={{height: "100%", float: "right", display: "flex", alignItems: "center"}}>
-        <Link to="/" style={{color: "white"}}>
-          <i className="fas fa-times fa-lg"></i>
-        </Link>
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div style={headerStyle}>
+        <div className="header-container" style={contentStyle}>
+          <Link to="/">
+            <img
+              style={{margin: 0, height: "100%"}}
+              src={require("../images/kv_transparent.svg")} alt="logo"/>
+          </Link>
+          <div style={{height: "100%", float: "right", display: "flex", alignItems: "center"}}>
+            {this.props.backButton ? (
+              <Link to={this.props.backButton} style={{color: "white", marginRight: "24px"}}>
+                <i className="fas fa-arrow-left fa-lg"></i>
+              </Link>
+            ) :
+              (null)
+            }
+            <Link to="/" style={{color: "white"}}>
+              <i className="fas fa-times fa-lg"></i>
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    );
+  }
+}
 
 export default Header;
